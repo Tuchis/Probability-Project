fbf347fa440fc8f10f61575d3907cc3e9b6c1b5d
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2022-09-19 00:32:49 +0300

    Hiding block in permutations tab

--- script.js
+++ script.js
@@ -49,5 +49,16 @@ function ButtonColor(id) {
   else{
     document.getElementById(id).style.background = "#e8e9ea"
     document.getElementById(id).value = "No"
-  };
+  }
+  if (id == "yesnoperm" && document.getElementById(id).value == "Yes"){
+      document.getElementById("multinom").style.display = "none";
+      document.getElementById("hider").style.display = "none";
+      document.getElementById("label2hider").style.display = "none";
+  }
+  if (id == "yesnoperm" && document.getElementById(id).value == "No"){
+            document.getElementById("multinom").style.display = "block";
+      document.getElementById("hider").style.display = "block";
+      document.getElementById("label2hider").style.display = "block";
+      document.getElementById("multinom").style.margin = "auto";
+  }
 }
