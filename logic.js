50e4feb6fcdfec67af9b317277a5ff1246ea4fbc
mr <92575094+yakorch@users.noreply.github.com>
2022-09-19 02:32:32 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -10,6 +10,10 @@ const comb_yes_no = document.getElementById("yesnocomb")
 const div_first = document.getElementById("1")
 const div_sec = document.getElementById("2")
 const div_third = document.getElementById("3")
+const div_forth = document.getElementById("4")
+const div_fifth = document.getElementById("5")
+const div_sixth = document.getElementById("6")
+
 
 
 first_butt.addEventListener("click", function () {
@@ -25,25 +29,27 @@ third_butt.addEventListener("click", function () {
 })
 
 
-let str_div_perm = "<span class='formula'>${ P }_n = n!$</span>"
-let str_div_p_boosted = "<span class='formula'>${ M }_n(x_1...x_i) =\frac{n!}{x_1...x_i}$</span>>"
+// let str_div_perm = "<span class='formula'>${ P }_n = n!$</span>"
+// let str_div_p_boosted = "<span class='formula'>${ M }_n(x_1...x_i) =\frac{n!}{x_1...x_i}$</span>>"
 
 
-let str_div_alloc = "<span class='formula'>$n...n = n^k$</span>"
-let str_div_alloc_2 = "<span class='formula'>$A_n^k = \frac{n!}{(n-k)!}$</span>"
+// let str_div_alloc = "<span class='formula'>$n...n = n^k$</span>"
+// let str_div_alloc_2 = "<span class='formula'>$A_n^k = \frac{n!}{(n-k)!}$</span>"
 
-let str_div_comb_1 = "<span class='formula'>${C}_n^k = \frac{n!}{(n-k)! k!}$</span>"
-let str_div_comb_2 = "<span class='formula'>${C}_{n+k-1}^{k-1} = \frac{(n+k-1)!}{n!(k-1)!}$</span>"
+// let str_div_comb_1 = "<span class='formula'>${C}_n^k = \frac{n!}{(n-k)! k!}$</span>"
+// let str_div_comb_2 = "<span class='formula'>${C}_{n+k-1}^{k-1} = \frac{(n+k-1)!}{n!(k-1)!}$</span>"
 
 function block_permutations() {
+    div_first.style.display = "none"
+    div_forth.style.display = "none"
     let answer = perm_yes_no.value
     let n = parseInt(document.getElementById("permelem").value)
     if (answer === "Yes") {
-        div_first.innerHTML = str_div_perm
+        div_first.style.display = "block"
         return factorial(n)
     }
     let values = get_all_similar(document.getElementById("multinom").value)
-    div_first.innerHTML = str_div_p_boosted
+    div_forth.style.display = "block"
     return permutations_replacement(n, values)
 }
 
@@ -54,26 +60,30 @@ function get_all_similar(string) {
 }
 
 function block_allocations() {
+    div_sec.style.display = "none"
+    div_fifth.style.display = "none"
     let answer = allo_yes_no.value
     let n = parseInt(document.getElementById("alocall").value)
     let k = parseInt(document.getElementById("alocnumber").value)
     if (answer === "Yes") {
-        div_sec.innerHTML = str_div_alloc
+        div_sec.style.display = "block"
         return n ** k
     }
-    div_sec.innerHTML = str_div_alloc_2
+    div_fifth.style.display = "block"
     return allocations(n, k)
 }
 
 function block_combinations() {
+    div_third.style.display = "none"
+    div_sixth.style.display = "none"
     let answer = comb_yes_no.value
     let n = parseInt(document.getElementById("comball"))
     let k = parseInt(document.getElementById("combgroupnum"))
     if (answer === "Yes") {
-        div_third.innerHTML = str_div_comb_1
+        div_third.style.display = "block"
         return combinations(n, k)
     }
-    div_third.innerHTML = str_div_comb_2
+    div_sixth.style.display = "block"
     return non_neg_integer_equation(k, n)
 }
 
